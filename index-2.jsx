7b774ecdd611a0f99d1c145e6ec677149aa5190d
LewisHong1 <48549605+LewisHong1@users.noreply.github.com>
2020-12-14 19:59:58 +0800

    Add files via upload

---
+++ index-2.jsx
@@ -0,0 +1,7 @@
+import React, {PureComponent} from 'react';
+import {connect} from 'dva';
+import {Breadcrumb, Button, Card, Col, DatePicker, Form, Radio, Row, Select, Table} from 'antd';
+import moment from 'moment'
+import { PageHeaderWrapper,  } from '@ant-design/pro-layout';
+import styles from '../report.less'
+
